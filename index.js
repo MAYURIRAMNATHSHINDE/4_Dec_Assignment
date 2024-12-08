67ab575b2f9f20a118166b16137a744d93146dea
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-12-08 20:27:17 +0530

    Q.no 2

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ const products = [
 ];
 
 processProducts(products);
+
+/***********************************  Q.No:2********************************** */
+
+function processStudents(students) {
+    // students who scored above 60 marks
+    const filteredStudents = students.filter(student => student.marks > 60);
+    // Sort the filtered array in descending order of marks
+    const sortedStudents = filteredStudents.sort((a, b) => b.marks - a.marks);
+    // Map the sorted array to extract only the names
+    const sortedNames = sortedStudents.map(student => student.name);
+
+    return sortedNames;
+}
+
+const students = [
+    { name: "Alice", marks: 58 },
+    { name: "Bob", marks: 85 },
+    { name: "Charlie", marks: 92 },
+    { name: "David", marks: 45 }
+];
+
+const result = processStudents(students);
+console.log(result);
