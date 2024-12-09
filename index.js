fed0d3f856e1bfcde6c54a480013aa86463c118a
MAYURIRAMNATHSHINDE <mayurishinde24304@gmail.com>
2024-12-09 12:37:12 +0530

    Q.no 4

--- index.js
+++ index.js
@@ -40,3 +40,36 @@ const students = [
 
 const result = processStudents(students);
 console.log(result);
+
+/************************************ Q. no 3************************************ */
+
+
+let employeeData=[{name:"Alice",tasksCompleted:9,rating:4.5,},
+                  {name:"Jack",tasksCompleted:12,rating:4.9,},
+                  {name:"Imily",tasksCompleted:5,rating:3.5,},
+                  {name:"July",tasksCompleted:6,rating:3.6,},
+                  {name:"Bob",tasksCompleted:15,rating:5.0,},
+                  {name:"Jems",tasksCompleted:7,rating:2.9,},
+                
+                ];
+
+let filterData = employeeData.filter((ele)=> ele.tasksCompleted>5);
+console.log(filterData);
+
+let newData=employeeData.map((ele)=>{
+    return {
+        name:ele.name,
+        PerformanceLevel:ele.rating>4.5?"Excellent":ele.rating>3&&ele.rating<=4.5?"Good":"Needs Improvement"
+    }
+})
+//console.log(newData)
+newData.sort((a,b)=>{
+    if(a.PerformanceLevel>b.PerformanceLevel){
+        return 1
+    }
+    else{
+        return -1
+
+    }
+})
+console.log(newData)
